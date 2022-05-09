ac3e10927408d3b68f18babeabb03d0dac61eafd
CGMacLean <c.g.maclean.182@gmail.com>
2022-05-09 09:24:47 +0100

    File function

--- File_functions.py
+++ File_functions.py
@@ -1,88 +1,74 @@
 
 
-def read_file(file_location):
-    try:
-        with open(file_location, 'r') as file:
-            file_line = [line.rstrip() for line in file]
-            for i, product in enumerate(file_line):
-                print(f'{i}:{product}')
-                # return print_lines
-
-    except:
-        print('error has occurred')
-    # finally:
-        # file.close()
-
-
-item_list = read_file('Product_list.txt')
-
-print(item_list)
-
-# file = open("hello.txt", "r")
-# contents = file.read()
-# print(contents)
-# file = open("people.txt", "r")
-# lines = file.readlines()
-# for line in line:
-# print(line)
-
-# def write_file(file_location):
-#     try:
-#         with open(file_location, 'w') as file:
-#             for line in lines:
-#                 file.write(item + '\n')
-#                 return lines
-#     except:
-#         print('error has occurred')
-
-# write_file('Product_list.txt')
-
-# items = ['apple', 'grapes', 'orange', 'banana']
-# with open('people.txt', 'w') as items_file:
-#     for item in items:
-#         items_file.write(item + '\n')
-
-# def add_product():
-#     file = open('Product_list.txt', 'a+')
-#     products_file = []
-#     product = input("type a product:")
-#     file.write(f'{product}\n')
-#     products_file.append(product)
-#     file.close()
-
-# add_product()
-
-# edit version 1
-# def edit_product():
-#     file = open('Product_list.txt', 'r+')
-#     product_list = file.read()
-#     user_edit = input('what do you want to edit?')
-#     user_replace = input('what do you want to replace with?')
-#     product_list = product_list.replace(user_edit, user_replace)
-#     file.close()
-
-#     file = open('Product_list.txt', 'w+')
-#     file.write(product_list)
-#     file.close()
-
-# edit_product()
+def print_logo():
+    print('''          
+          _.._..,_,_
+         (          )
+          ]~,"-.-~~[
+        .=])' (;  ([
+        | ]:: '    [
+        '=]): .)  ([
+          |:: '    |
+           ~~----~~
+    Callum Maclean Drinks
+    ''')
 
 
-# context managers
+def exit_to_menu():
+    print('return to main menu')
+    screen_refresh()
 
-# version 2
+
+def get_item_list(file_location):
+    with open(file_location, 'r', newline='') as file:
+        lines = [line.rstrip() for line in file.readlines()]
+        return lines
 
 
-# fin = open("data.txt", "r+")
-# #read file contents to string
-# data = fin.read()
-# #replace all occurrences of the required string
-# data = data.replace('pyton', 'python')
-# #close the input file
-# fin.close()
-# #open the input file in write mode
-# fin = open("data.txt", "w+")
-# #overrite the input file with the resulting data
-# fin.write(data)
-# #close the file
-# fin.close()
+def print_list(item_list):
+    for i, words in enumerate(item_list):
+        print(i, words)
+
+
+def add_item(file_location, item_name):
+    with open(file_location, 'a+') as file:
+        new_item = input(f'What {item_name} do you want to add?:').title()
+        file.write(f'{new_item}\n')
+        file.close()
+        return new_item
+
+
+def update_item(file_location, item_list, item_name):
+    with open(file_location, 'r+', newline='') as file:
+        lines = [line.rstrip() for line in file.readlines()]
+        for i, words in enumerate(lines):
+            print(i, words)
+        user_edit_input = int(
+            input(f"press the number of the {item_name} you want to edit?"))
+        user_replace_input = input(
+            'what do you want to replace with?:').title()
+        item_list[user_edit_input] = user_replace_input
+        screen_refresh()
+        return item_list
+
+
+def save(file_location, lists):
+    with open(file_location, "w") as file:
+        for i in lists:
+            file.write(f"{i}\n")
+
+
+def delete_item(file_location, item_list, item_name):
+    with open(file_location, 'r+', newline='') as file:
+        lines = [line.rstrip() for line in file.readlines()]
+        for i, words in enumerate(lines):
+            print(i, words)
+        user_delete_input = (
+            int(input(f"press the number of the {item_name} you want to delete?")))
+        del item_list[user_delete_input]
+
+    screen_refresh()
+
+
+def screen_refresh():
+    os.system('cls')
--- miniproject.py
+++ miniproject.py
@@ -1,83 +1,9 @@
 import os
 import time
-
+from File_functions import *
 
 PRODUCTS = 'product_list.txt'
 COURIERS = 'courier_list.txt'
-
-
-def print_logo():
-    print('''          
-          _.._..,_,_
-         (          )
-          ]~,"-.-~~[
-        .=])' (;  ([
-        | ]:: '    [
-        '=]): .)  ([
-          |:: '    |
-           ~~----~~
-    Callum Maclean Drinks
-    ''')
-
-
-def exit_to_menu():
-    print('return to main menu')
-    screen_refresh()
-
-
-def get_item_list(file_location):
-    with open(file_location, 'r', newline='') as file:
-        lines = [line.rstrip() for line in file.readlines()]
-        return lines
-
-
-def print_list(item_list):
-    for i, words in enumerate(item_list):
-        print(i, words)
-
-
-def add_item(file_location, item_name):
-    with open(file_location, 'a+') as file:
-        new_item = input(f'What {item_name} do you want to add?:').title()
-        file.write(f'{new_item}\n')
-        file.close()
-        return new_item
-
-
-def update_item(file_location, item_list, item_name):
-    with open(file_location, 'r+', newline='') as file:
-        lines = [line.rstrip() for line in file.readlines()]
-        for i, words in enumerate(lines):
-            print(i, words)
-        user_edit_input = int(
-            input(f"press the number of the {item_name} you want to edit?"))
-        user_replace_input = input(
-            'what do you want to replace with?:').title()
-        item_list[user_edit_input] = user_replace_input
-        screen_refresh()
-        return item_list
-
-
-def save(file_location, lists):
-    with open(file_location, "w") as file:
-        for i in lists:
-            file.write(f"{i}\n")
-
-
-def delete_item(file_location, item_list, item_name):
-    with open(file_location, 'r+', newline='') as file:
-        lines = [line.rstrip() for line in file.readlines()]
-        for i, words in enumerate(lines):
-            print(i, words)
-        user_delete_input = (
-            int(input(f"press the number of the {item_name} you want to delete?")))
-        del item_list[user_delete_input]
-
-    screen_refresh()
-
-
-def screen_refresh():
-    os.system('cls')
 
 
 print_logo()
