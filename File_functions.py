12369a5bf63278218f9dd947f6e8e4c5c77e0a83
CGMacLean <c.g.maclean.182@gmail.com>
2022-05-13 09:56:29 +0100

    finished order menu

--- File_functions.py
+++ File_functions.py
@@ -1,9 +1,11 @@
 import json
+import os
+import time
 # import yaml
 
 
 def print_logo():
-    print('''          
+    print('''
           _.._..,_,_
          (          )
           ]~,"-.-~~[
@@ -14,6 +16,11 @@ def print_logo():
            ~~----~~
     Callum Maclean Drinks
     ''')
+
+
+def refresh_screen():
+    os.system('cls')
+    print_logo()
 
 
 def exit_to_menu():
@@ -68,18 +75,125 @@ def delete_item(file_location, item_list, item_name):
         del item_list[user_delete_input]
 
 
-def load_order_list(item_list):
+def load_json(item_list):
     with open(item_list, 'r') as file:
         contents = json.loads(file.read())
 
-        return contents
+    return contents
 
 
-contents = load_order_list('order_list.json')
+def add_order_list(item_list, order_list, item_name):
+    with open(item_list, 'a+') as file:
+        new_item = input(f'What {item_name} do you want to add?:').title()
+        order_list.append(new_item)
+        file.write(f'{new_item}\n')
+        file.close()
+        return new_item
+
+
+# contents = load_json('order_list.json')
 
 
 def print_order_list(contents):
     print(contents)
 
 
-print_order_list(contents)
+def read_json(filename):
+    with open(filename, "r") as file:
+        data = json.load(file)
+        return data
+
+
+def input_new_order(order_list, courier_list):
+    new_orders = {}
+    new_orders['customer_name'] = input('type the customer name:')
+    new_orders['customer_address'] = input('type customer address:')
+    new_orders['customer_phone'] = input('type customer number:')
+    print_list(courier_list)
+    courier_index = int(input('pick the number of the courier:'))
+    new_orders['courier'] = int(courier_index)
+    new_orders['status'] = 'preparing'
+    return order_list.append(new_orders)
+
+
+def update_json(data, entry):
+    data.append(entry)
+
+
+def save_orders(lists, filename):
+    with open(filename,  "w") as file:
+        file.write(json.dumps(lists, indent=4))
+
+
+# edit value of key in dict
+def pick_order(order_list):
+    print_list(order_list)
+    order_index = int(input('pick the number of the order:'))
+    picked_order = order_list[order_index]
+    return picked_order
+
+
+def edit_order_status(picked_order):
+    refresh_screen()
+    print(f'"status":{picked_order["status"]}')
+    status_options = ['preparing', 'out for delivery', 'complete']
+    print_list(status_options)
+    picked_status = int(
+        input('press the number of the update you want to apply:'))
+    status_value = status_options[picked_status]
+    picked_order["status"] = status_value
+    print(f'Status has now changed to \n"status":{status_value}')
+    time.sleep(3)
+    refresh_screen()
+    return picked_order
+
+
+def edit_dict(picked_order, courier_list):
+    refresh_screen()
+    while True:
+        key_list = ['customer_name', 'customer_address',
+                    'customer_phone', 'courier', 'status']
+        print_list(key_list)
+
+        order_key_index = int(
+            input('pick the number of the option you want to edit:'))
+        key_choice = key_list[order_key_index]
+        key_value = picked_order[key_choice]
+        if order_key_index == 3:
+            print(f'{key_choice}:{key_value}')
+            print_list(courier_list)
+            courier_index = int(
+                input('pick the number of the courier you would like to replace with?:'))
+            picked_order['courier'] = courier_index
+            print(f'{key_choice}:{key_value}')
+            return picked_order
+        elif order_key_index == 4:
+            edit_order_status(picked_order)
+            return picked_order
+        elif order_key_index == 0 or 1 or 2:
+            refresh_screen()
+            print(f'{key_choice}:{key_value}')
+            picked_order[key_choice] = input(
+                'what would you like to replace with?:')
+            return picked_order
+        return picked_order
+
+
+def delete_order(order_list):
+    print_list(order_list)
+    order_index = int(input('pick the number of the order:'))
+    del order_list[order_index]
+    # list.pop(order_index)
+    return
+    # return order_list.remove(order_index)
+
+    # order_index = int(input('pick the number of the option you want to edit:'))
+    # # new_edit = picked_order[order_index]
+    # picked_order.append()
+    # print(new_edit)
+
+    # return new_edit
+
+
+# courier_index = int(input('pick the number of the courier:'))
+# new_orders['courier'] = courier_list[courier_index]
--- miniproject.py
+++ miniproject.py
@@ -1,20 +1,23 @@
 import os
 import time
-
 from File_functions import *
 
 PRODUCTS = 'product_list.txt'
 COURIERS = 'courier_list.txt'
 ORDERS = 'order_list.json'
-
-
-def refresh_screen():
-    os.system('cls')
-    print_logo()
+ORDER_STATUS = 'order_status.json'
 
 
 product_list = get_item_list(PRODUCTS)
 courier_list = get_item_list(COURIERS)
+order_list = load_json(ORDERS)
+order_status = load_json(ORDER_STATUS)
+
+# courier_by_index = {print_list(courier_list)}
+
+print_logo()
+
+
 # boot while loop
 while True:
     refresh_screen()
@@ -23,21 +26,26 @@ while True:
     if user_input == '0':
         save(PRODUCTS, product_list)
         save(COURIERS, courier_list)
+        save_orders(order_list, ORDERS)
         break
     elif user_input == '1':
+        # product menu
         refresh_screen()
         while True:
             user_menu_input = input(
-                'press 0 to go to main menu \npress 1 to view products \npress 2 add a product \npress 3 to edit product list \npress 4 to delete product:')
+                'press 0 to go to main menu: \npress 1 to view products: \npress 2 add a product: \npress 3 to edit product list: \npress 4 to delete product:')
             if user_menu_input == '0':
+                # RETURN to main menu
                 exit_to_menu()
                 refresh_screen()
                 break
             elif user_menu_input == '1':
+                # PRINT products list
                 print_list(product_list)
                 time.sleep(3)
                 refresh_screen()
             elif user_menu_input == '2':
+                # CREATE new product
                 print_list(product_list)
                 new_item = add_item(PRODUCTS, 'products')
                 product_list.append(new_item)
@@ -45,27 +53,33 @@ while True:
                 time.sleep(3)
                 refresh_screen()
             elif user_menu_input == '3':
+                # UPDATE existing product
                 product_list = update_item(PRODUCTS, product_list, 'products')
                 save(PRODUCTS, product_list)
                 refresh_screen()
             elif user_menu_input == '4':
+                # DELETE product
                 delete_item(PRODUCTS, product_list, 'products')
                 save(PRODUCTS, product_list)
                 refresh_screen()
     elif user_input == '2':
+        # courier menu
         refresh_screen()
         while True:
             user_menu_input = input(
-                'press 0 to go to main menu \npress 1 to view couriers \npress 2 add a courier \npress 3 to edit courier list \npress 4 to delete courier:')
+                'press 0 to go to main menu: \npress 1 to view couriers: \npress 2 add a courier: \npress 3 to edit courier list: \npress 4 to delete courier:')
             if user_menu_input == '0':
+                # RETURN to main menu
                 exit_to_menu()
                 refresh_screen()
                 break
             elif user_menu_input == '1':
+                # PRINT couriers list
                 print_list(courier_list)
                 time.sleep(3)
                 refresh_screen()
             elif user_menu_input == '2':
+                # CREATE new courier
                 print_list(courier_list)
                 new_item = add_item(COURIERS, 'courier')
                 courier_list.append(new_item)
@@ -73,6 +87,7 @@ while True:
                 time.sleep(3)
                 refresh_screen()
             elif user_menu_input == '3':
+                # UPDATE existing courier
                 courier_list = update_item(COURIERS, courier_list, 'courier')
                 save(COURIERS, courier_list)
                 refresh_screen()
@@ -81,13 +96,40 @@ while True:
                 save(COURIERS, courier_list)
                 refresh_screen()
     elif user_input == '3':
-        refresh_screen
+        # order menu
+        refresh_screen()
         while True:
+            refresh_screen()
             user_menu_input = input(
                 'press 0 to go to main menu \npress 1 to view orders \npress 2 add a order \npress 3 to edit order status \npress 4 to update order: \npress 5 to delete order:')
             if user_menu_input == '0':
                 exit_to_menu()
                 refresh_screen()
                 break
-            if user_menu_input == '1':
-                print_order_list(ORDERS)
+            elif user_menu_input == '1':
+                print_order_list(order_list)
+                time.sleep(3)
+                refresh_screen()
+            elif user_menu_input == '2':
+                input_new_order(order_list, courier_list)
+                save_orders(order_list, ORDERS)
+                print_order_list(order_list)
+                time.sleep(3)
+                refresh_screen()
+            elif user_menu_input == '3':
+                picked_order = pick_order(order_list)
+                edit_order_status(picked_order)
+                save_orders(order_list, ORDERS)
+                refresh_screen()
+            elif user_menu_input == '4':
+                refresh_screen()
+                picked_order = pick_order(order_list)
+                # print_list(picked_order)
+                edit_dict(picked_order, courier_list)
+                # new_edit = edit_dict(picked_order)
+                save_orders(order_list, ORDERS)
+                refresh_screen()
+            elif user_menu_input == '5':
+                delete_order(order_list)
+                save_orders(order_list, ORDERS)
+                refresh_screen()
